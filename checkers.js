5b2a5a2fbc168f26f2c7299fff242df1edf3936e
Arun P <arunparamanathan@ksu.edu>
2017-09-08 10:42:24 -0500

    Using code from class.
    Updated highlight code from class example.

--- checkers.js
+++ checkers.js
@@ -19,11 +19,6 @@ var state = {
   captures: {w: 0, b: 0}
 }
 
-// Keep track of the squares we highlighted last so we can
-// un-highlight them when click event is called again.
-// This prevents the squares from staying highlighted forever.
-var lastHighlightedSquares = [];
-
 /** @function getLegalMoves
   * returns a list of legal moves for the specified
   * piece to make.
@@ -39,12 +34,12 @@ function getLegalMoves(piece, x, y) {
     case 'b': // black can only move down the board diagonally
       checkSlide(moves, x-1, y-1);
       checkSlide(moves, x+1, y-1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
     case 'w':  // white can only move up the board diagonally
       checkSlide(moves, x-1, y+1);
       checkSlide(moves, x+1, y+1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
     case 'bk': // kings can move diagonally any direction
     case 'wk': // kings can move diagonally any direction
@@ -52,7 +47,7 @@ function getLegalMoves(piece, x, y) {
       checkSlide(moves, x+1, y+1);
       checkSlide(moves, x-1, y-1);
       checkSlide(moves, x+1, y-1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
   }
   return moves;
@@ -83,6 +78,8 @@ function copyJumps(jumps) {
   // Use Array.prototype.slice() to create a copy
   // of the landings and captures array.
   var newJumps = {
+    x: jumps.x,
+    y: jumps.y,
     landings: jumps.landings.slice(),
     captures: jumps.captures.slice()
   }
@@ -135,13 +132,17 @@ function checkJump(moves, jumps, piece, x, y) {
   * @param {integer} ly - the 'landing' y position of the peice is jumping onto
   */
 function checkLanding(moves, jumps, piece, cx, cy, lx, ly) {
+  // Check that we're not jumping back to our starting position
+  if(lx == jumps.x && ly == jumps.y) return;
   // Check landing square is on grid
   if(lx < 0 || lx > 9 || ly < 0 || ly > 9) return;
   // Check landing square is unoccupied
   if(state.board[ly][lx]) return;
-  // Check capture square is occuped by opponent
-  if((piece === 'b' || piece === 'bk') && !(state.board[cy][cx] === 'w' || state.board[cy][cx] === 'wk')) return;
-  if((piece === 'w' || piece === 'wk') && !(state.board[cy][cx] === 'b' || state.board[cy][cx] === 'bk')) return;
+  // Check capture square is occupied by opponent
+  if(state.turn === 'b' && !(state.board[cy][cx] === 'w' || state.board[cy][cx] === 'wk')) return;
+  if(state.turn === 'w' && !(state.board[cy][cx] === 'b' || state.board[cy][cx] === 'bk')) return;
+  // Check that we haven't landed on this square previously
+  if(0 < jumps.landings.indexOf(function(landing){return landing.x == lx && landing.y == ly;})) return;
   // legal jump! add it to the moves list
   jumps.captures.push({x: cx, y: cy});
   jumps.landings.push({x: lx, y: ly});
@@ -203,38 +204,57 @@ function nextTurn() {
   else state.turn = 'b';
 }
 
-function handleCheckerClick(event) {
-  for (var i = 0; i < lastHighlightedSquares.length; i++){
-    lastHighlightedSquares[i].classList.remove('highlight');
-  }
+/** @function clearHighlights
+  * Clears all highligted squares
+  */
+function clearHighlights() {
+  var highlighted = document.querySelectorAll('.highlight');
+  highlighted.forEach(function(square){
+    square.classList.remove('highlight');
+  });
+}
 
+/** @function handleCheckerClick
+  * Click handler for checker
+  */
+function handleCheckerClick(event) {
   event.preventDefault();
   var parentId = event.target.parentElement.id;
   var x = parseInt(parentId.charAt(7));
   var y = parseInt(parentId.charAt(9));
+  var piece = state.board[y][x];
+  // Clear old highlights
+  clearHighlights();
+  // Make sure the checker is the player's
+  if(piece.charAt(0) !== state.turn) return;
+  // Get legal moves
   var moves = getLegalMoves(state.board[y][x], x, y);
-
-  // Highlight squares you can move to.
-  for (var m = 0; m < moves.length; m++) {
-    var possibleMove = moves[m];
-    var square = document.getElementById('square-' + possibleMove.x + '-' + possibleMove.y);
-    square.classList.add('highlight');
-    lastHighlightedSquares.push(square);
-  }
+  // mark checker to move
+  event.target.classList.add('highlight');
+  // Mark squares available for moves
+  moves.forEach(function(move){
+    if(move.type === 'slide') {
+      var square = document.getElementById('square-' + move.x + '-' + move.y);
+      square.classList.add('highlight');
+    }
+  })
 }
 
+/** @function setup
+  * Sets up the game environment
+  */
 function setup() {
   var board = document.createElement('section');
   board.id = 'game-board';
   document.body.appendChild(board);
-  for (var y = 0; y < state.board.length; y++) {
-    for(var x = 0; x < state.board[y].length; x++) {
+  for(var y = 0; y < state.board.length; y++){
+    for(var x = 0; x < state.board[y].length; x++){
       var square = document.createElement('div');
       square.id = "square-" + x + "-" + y;
       square.classList.add('square');
-      if ((y+x) % 2 != 0) square.classList.add('black');
+      if((y+x) % 2 === 1) square.classList.add('black');
       board.appendChild(square);
-      if (state.board[y][x]) {
+      if(state.board[y][x]) {
         var checker = document.createElement('div');
         checker.classList.add('checker');
         checker.classList.add('checker-' + state.board[y][x]);
@@ -246,38 +266,3 @@ function setup() {
 }
 
 setup();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// End.
